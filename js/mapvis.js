64606f01074b39106e3c2a32e85969026eed60dd
Wentao Xu <contactwentaoxu@gmail.com>
2015-05-03 03:04:03 -0400

    Object Oriented Completed

--- js/mapvis.js
+++ js/mapvis.js
@@ -15,7 +15,7 @@ MapVis.prototype.initVis = function(){
 	this.svg = this.parentElement.selectAll("svg");
 	this.margin = {top:20, right:20, bottom:20, left:20};
 	this.width = this.svg.attr("width")-this.margin.left - this.margin.right;
-	this.height = this.attr("height")-this.margin.top - this.margin.bottom;
+	this.height = this.svg.attr("height")-this.margin.top - this.margin.bottom;
 
 	this.svg = this.svg.append("g")
 		.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
@@ -31,62 +31,67 @@ MapVis.prototype.initVis = function(){
     this.path = d3.geo.path()
     	.pointRadius([0.5])
     	.projection(this.projection)
-
+   	
+   	var that = this;
     var zoom = d3.behavior.zoom()
-    	.translate(projection.translate())
-    	.scale(projection.scale())
-    	.scaleExtent([height, 8 * height])
-    	.on("zoom", zoomed);
+    	.translate(this.projection.translate())
+    	.scale(this.projection.scale())
+    	.scaleExtent([this.height, 8 * this.height])
+    	.on("zoom", function(){that.zoomed(that)});
 	
 	this.g = this.svg.append("g")
-    	.call(zoom);
+    	.call(zoom);	
+    this.updateVis();
 }
 
 MapVis.prototype.updateVis = function(){
+	var that = this;
 	this.g.append("g")
 		.attr("id", "states")
       	.selectAll("path")
         .data(topojson.feature(this.mapData, this.mapData.objects.states).features)
       	.enter().append("path")
-        .attr("d", path)
-        .on("click", clicked);
-
+        .attr("d", this.path)
+        .on("click", function(d){that.clicked(d, that)});
+    
  	this.g.append("path")
     	.datum(topojson.mesh(this.mapData, this.mapData.objects.states, function(a, b) { return a !== b; }))
     	.attr("id", "state-borders")
-    	.attr("d", path);
-
+    	.attr("d", this.path);
+	
 	this.g.append("g")
 		.attr("id", "hospitals")
 		.selectAll("path")
 		.data(this.hosData.features)
 		.enter().append("path")
-		.attr("d", path)
-		.on("click", display)  
+		.attr("d", this.path)
+		.on("click", this.display);  
+	
 }
 
-MapVis.prototype.clicked = function(d){
+MapVis.prototype.clicked = function(d, that){
+	
 	var x, y, k;
-	if (d && centered !== d){
-		var centroid = this.path.centroid(d);
+	if (d && that.centered !== d){
+		var centroid = that.path.centroid(d);
 		x = centroid[0]
 		y = centroid[1]
 		k = 4;
-		centered = d;
+		that.centered = d;
 	}
 	else{
-		x = this.width/2;
-		y = this.height/2;
+		x = that.width/2;
+		y = that.height/2;
 		k = 1;
-		centered = null;
+		that.centered = null;
 	}
 
-	this.g.selectAll("path")
-		.classed("active", centered && function(d){ return d === centered;});
+	that.g.selectAll("path")
+		.classed("active", that.centered && function(d){ return d === that.centered;});
 
-	this.g.transition()
+	that.g.transition()
 		.duration(750)
-      	.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      	.attr("transform", "translate(" + that.width / 2 + "," + that.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
       	.style("stroke-width", 1.5 / k + "px");
 
 }
@@ -95,9 +100,9 @@ MapVis.prototype.display = function(d){
 	console.log(d);
 }
 
-MapVis.prototype.zoomed = function(){
-	this.projection.translate(d3.event.translate).scale(d3.event.scale);
-	g.selectAll("path").attr("d", path);
+MapVis.prototype.zoomed = function(that){
+	that.projection.translate(d3.event.translate).scale(d3.event.scale);
+	that.g.selectAll("path").attr("d", that.path);
 }
 
 
