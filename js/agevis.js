e002fcf00f116fcb02d1483d0b12fdfce31b460b
Wentao Xu <contactwentaoxu@gmail.com>
2015-05-04 01:21:22 -0400

    Both county and state demographics are split

--- js/agevis.js
+++ js/agevis.js
@@ -1,11 +1,11 @@
 AgeVis = function(_parentElement, _county_age, _state_demo, _eventHandler){
-	this.parentElement = _parentElement;
-	this.county_age = _county_age;
-	this.state_demo = _state_demo;
-	this.eventHandler = _eventHandler;
-	this.displayData = [];
-	this.age_domain = ["Under 19", "19 to 64", "Over 65"]
-	this.initVis();
+  this.parentElement = _parentElement;
+  this.county_age = _county_age;
+  this.state_demo = _state_demo;
+  this.eventHandler = _eventHandler;
+  this.displayData = [];
+  this.age_domain = ["Under 19", "19 to 64", "Over 65"]
+  this.initVis();
 }
 
 AgeVis.prototype.initVis = function(){
@@ -27,7 +27,8 @@ AgeVis.prototype.initVis = function(){
       .domain(d3.range(0, that.age_domain.length));
 
     this.y = d3.scale.linear()
-      .range([this.height - 150,0]);
+      .range([this.height - 50, 0])
+      .domain([0, 100]);
 
     this.xAxis = d3.svg.axis()
       .scale(this.x)
@@ -41,7 +42,7 @@ AgeVis.prototype.initVis = function(){
     // Add axes visual elements
     this.svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + (this.height - 150) + ")")
+        .attr("transform", "translate(0," + (this.height - 50) + ")")
         .call(this.xAxis)
         .selectAll("text")
           .attr("y", 5)
@@ -50,7 +51,7 @@ AgeVis.prototype.initVis = function(){
           .attr("text-anchor",  "start")
           .style("text-anchor", "start")
           .text(function(d,i) { return that.age_domain[i]})
-          .attr("font-size", 8);
+          .attr("font-size", 12);
 
     this.svg.append("g")
         .attr("class", "y axis")
@@ -60,47 +61,47 @@ AgeVis.prototype.initVis = function(){
 }
 
 AgeVis.prototype.updateVis = function(){
-	var that = this;
-
-	// set y scale domain to max value of count aggregate
-    this.y.domain([0, d3.max(this.displayData)]);
-
+  var that = this;
 
     // calls y Axis
     this.svg.select(".y.axis")
         .call(this.yAxis)
 
+    var tempdata = [];
+
+    tempdata.push(this.displayData["under_19"]);
+    tempdata.push(this.displayData["19_64"]);
+    tempdata.push(this.displayData["over_65"]);
+    
      // Data join
     var bar = this.svg.selectAll(".bar")
-      .data(this.displayData);
+      .data(tempdata);
 
     // removes unneeded bars
     bar.exit()
       .remove();
 
-    // Append new bar groups, if required
+    // Update the data
     bar.enter()
         .append("rect")
         .attr("class", "bar")
         .attr("x", function(d,i){
-            return  that.x(i);
+           return  that.x(i);
         })
-       .attr("width", (that.width/3));
+        .attr("width", (that.width/5));
 
-    // adds bar features
-    bar
-        .attr("y", function(d) {
-            return (that.y(d));
+    bar.attr("y", function(d) {
+            return (that.y(d))
         })
-       .attr("height", function(d){return that.height - 150 - that.y(d);})
+        .attr("height", function(d){return that.height - 50 - that.y(d);});
 
 }
 
 AgeVis.prototype.onSelectionChange = function(d){
-	var that = this;
-	var county = d.properties.county.toLowerCase()
-	this.displayData = that.county_age[county]
-	this.updateVis();
+  var that = this;
+  var county = d.properties.county.toLowerCase()
+  this.displayData = that.county_age[county]
+  this.updateVis();
 }
 
 
--- processing/split_demo.py
+++ processing/split_demo.py
@@ -3,35 +3,63 @@ import copy
 from collections import OrderedDict
 
 age_array = OrderedDict()
-age_file = open("../data/county_age.json", "w")
-age_target = ["under_19", "19_64", "65_84", "over_85"]
+age_file = open("../data/state_age.json", "w")
+age_target = ["under_18", "19_64", "65_74", "over_75"]
 
-with open("../data/county_demographics.json") as demo_file:
+with open("../data/state_demo.json") as demo_file:
 	demo = json.load(demo_file)
 
 """
 Retrieve numbers from the age target
 """
 counter = 0
-while counter < 4:
+while counter < len(age_target):
 	for key1, val1 in demo.iteritems():
 		if counter == 0:
 			age_array[key1] = OrderedDict()
 		for key2, val2 in demo[key1].iteritems():
 			if key2 == age_target[counter]:
-				age_array[key1][key2] = float(val2)
+				try:
+					age_array[key1][key2] = round((float(val2)/float(demo[key1]["total"]))*100, 1)
+				except KeyError:
+					continue
 	counter+=1
-
-
-"""
-Combine 65_84 and over_85
-"""
 
 final_age = copy.deepcopy(age_array)
 for key, val in age_array.iteritems():
-	final_age[key]["over_65"] = round(final_age[key]["65_84"] + final_age[key]["over_85"], 1)
-	del final_age[key]["65_84"]
-	del final_age[key]["over_85"]
+	final_age[key]["over_65"] = round((final_age[key]["65_74"] + final_age[key]["over_75"]), 1)
+	del final_age[key]["65_74"]
+	del final_age[key]["over_75"]
 
 json.dump(final_age, age_file, indent=4)
 
+race_array = OrderedDict()
+race_file = open("../data/state_race.json", "w")
+race_target = ["black", "hispanic", "white", "other"]
+
+with open("../data/state_demo.json") as demo_file:
+	demo = json.load(demo_file)
+
+counter = 0
+while counter <  len(race_target):
+	for key1, val1 in demo.iteritems():
+		if counter == 0:
+			race_array[key1] = OrderedDict()
+		for key2, val2 in demo[key1].iteritems():
+			if key2 == race_target[counter]:
+				try:
+					if val2 == "N/A":
+						race_array[key1][key2] = None
+					else:
+						race_array[key1][key2] = round((float(val2)/float(demo[key1]["total"]))*100, 1)
+				except KeyError:
+					continue
+	counter+=1
+
+# final_race = copy.deepcopy(race_array)
+# for key, val in race_array.iteritems():
+# 	final_race[key]["other"] = round(final_race[key]["asian"] + final_race[key]["native"], 1)
+# 	del final_race[key]["asian"]
+# 	del final_race[key]["native"]
+
+json.dump(race_array, race_file, indent=4)
