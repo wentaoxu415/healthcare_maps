95847dc50292b7846104d01c7bec8c98c2bf64ac
Wentao Xu <contactwentaoxu@gmail.com>
2015-05-05 02:55:32 -0400

    update

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
-QualityVis = function(_parentElement, _qualityData, _eventHandler){
+SpendingVis2 = function(_parentElement, _spendingData, _eventHandler){
   this.parentElement = _parentElement;
-  this.qualityData = _qualityData;
+  this.spendingData = _spendingData;
   this.eventHandler = _eventHandler;
   this.displayData = [];
   this.initVis();
 }
 
-QualityVis.prototype.initVis = function(){
+SpendingVis2.prototype.initVis = function(){
     var that = this; 
 
     // selects svg, sets parameters
@@ -15,31 +15,31 @@ QualityVis.prototype.initVis = function(){
     this.width = this.svg.attr("width") - this.margin.left - this.margin.right;
     this.height = this.svg.attr("height") - this.margin.top - this.margin.bottom;
 
-    for (var key in this.qualityData){
-        if (this.qualityData[key]){
-            this.displayData.push(this.qualityData[key])
-        }
+    for (var key in this.spendingData){
+    	if (this.spendingData[key]){
+    		this.displayData.push(this.spendingData[key])
+    	}
     }
-    console.log(d3.max(this.displayData, function(d){return d;}));
-    console.log(d3.min(this.displayData, function(d){return d;}));
+    
     this.svg = this.svg
-        .append("g") 
-        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    	.append("g") 
+    	.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
     // creates axis and scales
     this.x = d3.scale.linear()
-      .domain([d3.min(this.displayData, function(d){return d;}), d3.max(this.displayData, function(d){return d;})])
+      .domain([-30, 30])
       .range([0, this.width-50]);
 
-    var hist = d3.layout.histogram()
-        .bins(this.x.ticks(50))
-        (this.displayData);
+    
 
-    console.log(hist[0].dx);
+    var hist = d3.layout.histogram()
+    	.bins(this.x.ticks(50))
+    	(this.displayData);
+
     this.y = d3.scale.linear()
-        .domain([0, 1000])
-        .range([this.height-50, 0]);
-        
+    	.domain([0, 1000])
+      	.range([this.height-50, 0]);
+		
     this.xAxis = d3.svg.axis()
       .scale(this.x)
       .orient("bottom");
@@ -52,27 +52,26 @@ QualityVis.prototype.initVis = function(){
     // //Add bars
 
     this.bar = this.svg.selectAll(".bar")
-        .data(hist)
-        .enter().append("g")
-        .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + that.x(d.x) + "," + that.y(d.y) + ")"; });
+    	.data(hist)
+    	.enter().append("g")
+    	.attr("class", "bar")
+    	.attr("transform", function(d) { return "translate(" + that.x(d.x) + "," + that.y(d.y) + ")"; });
 
     this.bar.append("rect")
-        .attr("x", 1)
-        .attr("width", (that.width-50)/50-3.5)
-        .attr("height", function(d){ return that.height - 50 - that.y(d.y)});
+    	.attr("x", 1)
+    	.attr("width", this.x(hist[1].dx)-1)
+    	.attr("height", function(d){ return that.height - 50 - that.y(d.y)});
 
     
     var formatCount = d3.format(",.0f");
 
     this.bar.append("text")
-        .attr("dy", ".75em")
-        .attr("y", -20)
-        // .attr("x", this.x(hist[0].dx) / 2)
-        .attr("x", 5)
-        .attr("text-anchor", "middle")
-        .text(function(d) { return formatCount(d.y); })
-        .style("color", "#FFF");
+    	.attr("dy", ".75em")
+    	.attr("y", -20)
+    	.attr("x", this.x(hist[0].dx) / 2)
+    	.attr("text-anchor", "middle")
+    	.text(function(d) { return formatCount(d.y); })
+    	.style("color", "#FFF");
     // // Add axes visual elements
     // this.svg.append("g")
     //     .attr("class", "x axis")
@@ -93,7 +92,8 @@ QualityVis.prototype.initVis = function(){
     //     .attr("transform", "rotate(-90)")
 }
 
-QualityVis.prototype.onSelectionChange = function(d){
-    var that = this;
+SpendingVis2.prototype.onSelectionChange = function(d){
+	var that = this;
+	console.log(d);	
 }
 
--- processing/quality.py
+++ processing/quality.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import numpy as np
+import copy
 from math import sqrt
 from collections import OrderedDict
 
@@ -107,42 +108,46 @@ with open(complication_file, 'rU') as complication_fh:
 			scores[row[1]] = 0
 		if "Not Available" not in row[12]:
 			if row[9] == "COMP_HIP_KNEE":
-				scores[row[1]] += (row[12] - means[0])/std[0]
+				scores[row[1]] += round((float(row[12]) - means[0])/std[0], 2)
 			elif row[9] == "MORT_30_AMI":
-				scores[1].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[1])/std[1], 2)
 			elif row[9] == "MORT_30_COPD":
-				scores[2].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[2])/std[2], 2)
 			elif row[9] == "MORT_30_HF":
-				scores[3].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[3])/std[3], 2)
 			elif row[9] == "MORT_30_PN":
-				scores[4].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[4])/std[4], 2)
 			elif row[9] == "MORT_30_STK":
-				scores[5].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[5])/std[5], 2)
 			elif row[9] == "PSI_12_POSTOP_PULMEMB_DVT":
-				scores[6].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[6])/std[6], 2)
 			elif row[9] == "PSI_14_POSTOP_DEHIS":
-				scores[7].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[7])/std[7], 2)
 			elif row[9] == "PSI_15_ACC_LAC":
-				scores[8].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[8])/std[8], 2)
 			elif row[9] == "PSI_4_SURG_COMP":
-				scores[9].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[9])/std[9], 2)
 			elif row[9] == "PSI_6_IAT_PTX":
-				scores[10].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[10])/std[10], 2)
 			elif row[9] == "PSI_90_SAFETY":
-				scores[11].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[11])/std[11], 2)
 			elif row[9] == "READM_30_AMI":
-				scores[12].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[12])/std[12], 2)
 			elif row[9] == "READM_30_COPD":
-				scores[13].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[13])/std[13], 2)
 			elif row[9] == "READM_30_HF":
-				scores[14].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[14])/std[14], 2)
 			elif row[9] == "READM_30_HIP_KNEE":
-				scores[15].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[15])/std[15], 2)
 			elif row[9] == "READM_30_HOSP_WIDE":
-				scores[16].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[16])/std[16], 2)
 			elif row[9] == "READM_30_PN":
-				scores[17].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[17])/std[17], 2)
 			elif row[9] == "READM_30_STK":
-				scores[18].append(float(row[12]))
+				scores[row[1]] += round((float(row[12]) - means[18])/std[18], 2)
 
-
+final_scores = OrderedDict()
+for key, val in scores.iteritems():
+	final_scores[key] = round(val, 2)
+	
+json.dump(final_scores, out_file, indent=4)
