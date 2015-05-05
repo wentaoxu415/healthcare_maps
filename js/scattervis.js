ecd803e15a9025bedd24d2f5b78f9ac712f5b5a8
Wentao Xu <contactwentaoxu@gmail.com>
2015-05-05 19:44:03 -0400

    Fixed Scatterplot

--- js/scattervis.js
+++ js/scattervis.js
@@ -37,11 +37,11 @@ ScatterVis.prototype.initVis = function () {
         .range([0, this.height]);
 
     this.xAxis = d3.svg.axis()
-        // .scale(this.x)
+        .scale(this.x)
         .orient("bottom");
 
     this.yAxis = d3.svg.axis()
-        .scale(this.y)
+        // .scale(this.y)
         .orient("left");
 
     this.svg = this.svg.append("g")
