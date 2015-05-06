c17949b1b142eded209d90aefa16e2f4355228dc
Wentao Xu <contactwentaoxu@gmail.com>
2015-05-06 00:24:13 -0400

    test

--- js/spendingvis.js
+++ js/spendingvis.js
@@ -201,6 +201,7 @@ SpendingVis.prototype.onMapSelectionHighlight = function (d) {
     var hospital = d.el.properties.name;
     this.spending = d.highlight ? this.spendingData[hospital] : null;
     if (d.highlight) {
+        console.log("haha");
         this.tipData = hospital + '<br/>' + '$'+this.spendingData[hospital];
     }
     this.updateVis();
